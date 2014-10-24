e1c7cb7a9fcdc4b10a78440d2d5b856e9cfea8d4
Eugenio Sánchez <sanchezgarzae@gmail.com>
2014-10-23 21:35:18 -0500

    Avance 9:35

---
+++ Codigo/throughCode/src/mx/itesm/throughcode/Interfaz.java
@@ -0,0 +1,13 @@
+package mx.itesm.throughcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Interfaz extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_interfaz);
+	}
+}
